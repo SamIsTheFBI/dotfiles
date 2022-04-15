86acbaabf8cc6f632a702bde37679635f161e579
Shivam <70562711+SamIsTheFBI@users.noreply.github.com>
2022-04-16 04:49:02 +0530

    deleted pywal cache since no longer needed here

--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#1b1818";
-static const char* selfgcolor   = "#ede6de";
-static const char* normbgcolor  = "#61909E";
-static const char* normfgcolor  = "#ede6de";
-static const char* urgbgcolor   = "#D69E60";
-static const char* urgfgcolor   = "#ede6de";
