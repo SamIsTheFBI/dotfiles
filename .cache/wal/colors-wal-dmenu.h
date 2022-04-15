24a3b7db1c2623029ec0a81dba4293d1415e59e9
SamIsTheFBI <mrqube001@gmail.com>
2022-04-16 05:17:37 +0530

    ~

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#ede6de", "#1b1818" },
-	[SchemeSel] = { "#ede6de", "#D69E60" },
-	[SchemeOut] = { "#ede6de", "#BBBBC2" },
-};
